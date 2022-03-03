9d778d52ef90777e34588547a4a2c9cda2ba93bf
andtptit <andtptit@gmail.com>
2022-03-04 00:14:31 +0700

    add base

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <p className="mt-2"> Copyright AcadOnline &copy; 2021</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/config/fbConfig.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/functions'
+import 'firebase/auth';
+import 'firebase/analytics';
+import 'firebase/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9RyJosy_E93imr1ZNy8_RhDnSmNJsgZs",
+    authDomain: "flash-card-kid.firebaseapp.com",
+    projectId: "flash-card-kid",
+    storageBucket: "flash-card-kid.appspot.com",
+    messagingSenderId: "901379712581",
+    appId: "1:901379712581:web:9f7535ace6dbf34aeca52c",
+    measurementId: "G-GVDP6YNN7K"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const storage = firebase.storage();
+const functions = firebase.functions();
+
+export {
+    storage,
+    functions,
+    firebase as default
+}
