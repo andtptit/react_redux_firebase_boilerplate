ec72d2d20b1c962a9d4969cdc22694ee26b179c4
andtptit <andtptit@gmail.com>
2022-08-18 22:42:57 +0700

    update

--- src/Components/Cards/FlashCardArray.js
+++ src/Components/Cards/FlashCardArray.js
@@ -89,6 +89,7 @@ const FlashCardArray = ({course, profile, dataCourse, addLearned}) => {
 
   const [currentCard, setCurrentCard] = useState({})
   const [cardNum, setCardNum] = useState(0)
+  const [isFlip, setIsFlip] = useState(true)
 
   const handlePrevCard = () => {
     arrayRef.current.prevCard()
@@ -103,6 +104,11 @@ const FlashCardArray = ({course, profile, dataCourse, addLearned}) => {
     }
   }
   const arrayRef = useRef({});
+
+  const handleChangeClass = () => {
+    setIsFlip(!isFlip)
+  }
+
   return (
     <Container className="mt-4 mb-4">
      <h1 className="table-title mt-3 mb-3">FlashCard</h1>
@@ -117,25 +123,25 @@ const FlashCardArray = ({course, profile, dataCourse, addLearned}) => {
             <Progress className='mt-2' value={percent.toFixed(2)} />
             <div className="text-center">Tiến độ {percent.toFixed(2)}%</div>
           </Col>
-          <Col md='12'>
-            <Card className="mt-2 ">
-              <FlashcardArray
-                cards={arr}
-                count={true}
-                forwardRef={arrayRef}
-                control={false}
-                FlashCardClassName="FlashcardWrapper__item--back"
-                setCurrentCard={(currentCard) => {
-                  setCurrentCard(currentCard)
-                }}
-                onCardChange={(cardNumber)=>{
-                  setCardNum(cardNumber)
-                }}
-              />
-            </Card>
-            <Button color="primary" type="submit" className="mt-4 mr-4" onClick={() => handlePrevCard()}>Prev</Button>
-            <Button color="primary" type="submit" className="mt-4" onClick={() => handleNextCard()}>Next</Button>
-          </Col>
+
+          {/* Flashcard */}
+          <div className="col-md-6 cardContainer">
+            <div className={isFlip ? 'card' : 'card flipped'} onClick={() => handleChangeClass()}>
+              
+              <div className="front">
+                <h3 className="cardTitle">Front end (Mr. Cock - <strong>the bird</strong>)</h3>
+                <p>Markup and web languages such as HTML, CSS, JavaScript, and ancillary libraries commonly used in those languages such as Sass or JQuery.</p>
+              </div>
+              
+              <div className="back">
+                  <h3 className="cardTitle">Back end (Mr. Warm)</h3>
+                  <p>Scripting languages like Node.js, PHP, Python, Ruby, or Perl or Compiled languages like Go.</p>
+              </div>
+              
+            </div>
+          </div>
+
+          
       </Row>
 
       <Row md="12">
